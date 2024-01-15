11f2139f69d0b4567536a4147b0f8850539706a3
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-15 22:51:46 +0600

    lakjdfl;

--- function-object-recap.js
+++ function-object-recap.js
@@ -9,3 +9,20 @@ SIX CORE CONCEPTS
 6. Object (declare, properties, keys, values, et prop value, set value, loop object properties)
 
 */
+
+/*
+    1. google: function declaration vs function expression
+    2. google: arrow function
+    3. google: loop vs function
+    4. array vs object
+    5. integer vs float
+    6. == vs ===
+    7. differences between: var, let, const
+
+*/
+/*
+    1. celsius ---> Farenhite
+    2. Farenhite ---> Celsius
+    3. Simple Interest: Principal, interest rate, time ---> interest amount
+    4. Complex Interest:
+*/
