02f9892edff6818cf85ad1166924b734530196e0
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-15 22:36:40 +0600

    lakjdfl;

---
+++ function-object-recap.js
@@ -0,0 +1,11 @@
+/*
+--------------------
+SIX CORE CONCEPTS
+1. variable (declare, naming convension, variable types, primitive, math operations )
+2. Conditionals (comparison: >, <, >=, <=, ===, !==, if-else, if-else, if-else)
+3. Array (declare, index, get values, set values, indexOf, push, pop)
+4. Loop (for loop, while loop, break, continue)
+5. function(declare, parameters, return, call, use the returned value from a function)
+6. Object (declare, properties, keys, values, et prop value, set value, loop object properties)
+
+*/
